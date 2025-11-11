b631544db80c198e7c7a238933203508cac81309
Max Rodrigues <maxuel.rodrigues@gmail.com>
2025-11-11 19:11:21 -0300

    [feat] ✨: adicionado arquivos para translate

---
+++ lang/pt-br/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'failed' => '',
+    'password' => '',
+    'throttle' => '',
+
+    'login_title' => 'Login Painel de Controle',
+    'login_subtitle' => 'Por favor, insira suas credenciais para acessar o painel de controle.',
+
+    'username_field' => 'E-mail',
+    'password_field' => 'Senha',
+
+    'username_required' => 'O campo usuário é obrigatório',
+    'password_required' => 'O campo senha é obrigatório',
+];
