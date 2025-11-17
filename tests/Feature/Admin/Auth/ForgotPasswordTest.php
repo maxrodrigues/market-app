7e347993acd988a0020ef2773a460e13c7381cc2
Max Rodrigues <maxuel.rodrigues@gmail.com>
2025-11-17 18:47:01 -0300

    [test] 🧪: adicionados todos para criar nova rotina

---
+++ tests/Feature/Admin/Auth/ForgotPasswordTest.php
@@ -0,0 +1,13 @@
+<?php
+
+# FIRST STEP
+todo("has forgot password page");
+todo("return error when password doesnt exists");
+todo("reset user password");
+todo("create temporary recover link");
+todo("send email when found user");
+
+# SECOND STEP
+todo("return error when recover link is invalid");
+todo("return error when password is invalid");
+todo("redirect user when recover password");
