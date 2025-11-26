a803de6a38f515b1f950c5bf4c347e3af9284d9c
Max Rodrigues <maxuel.rodrigues@gmail.com>
2025-11-26 19:13:46 -0300

    [test] 🧪: teste automatizado de feature

--- tests/Feature/Admin/Auth/ForgotPasswordTest.php
+++ tests/Feature/Admin/Auth/ForgotPasswordTest.php
@@ -1,25 +1,41 @@
 <?php
 
 # FIRST STEP
+use Carbon\Carbon;
 use Symfony\Component\HttpFoundation\Response;
 
 it ("has forgot password page", function () {
-    $this->get(route('admin.reset-password'))
+    $this->get(route('admin.forgot-password.get'))
         ->assertStatus(Response::HTTP_OK);
 });
 
 it ("return error when user doesnt exists", function () {
-    $user = \App\Models\User::factory()->create();
-    $this->post(route('admin.reset-password'), ['email' => 'testemail@test.com'])
+    $this->post(route('admin.forgot-password.post'), ['email' => 'testemail@test.com'])
         ->assertSessionHasErrors(['not_found'])
         ->assertStatus(Response::HTTP_FOUND);
 });
 
 it ("reset user password", function () {
+    $user = \App\Models\User::factory()->create();
+    $this->post(route('admin.forgot-password.post'), ['email' => $user->email]);
 
+    $this->assertDatabaseCount('password_reset_tokens', 1);
+    $this->assertDatabaseHas('password_reset_tokens', ['email' => $user->email]);
 });
 
-todo("create temporary recover link");
+it ("create temporary recover link", function () {
+    $user = \App\Models\User::factory()->create();
+    $this->post(route('admin.forgot-password.post'), ['email' => $user->email]);
+
+    $resetTokens = \Illuminate\Support\Facades\DB::table('password_reset_tokens')
+        ->where('email', $user->email)
+        ->select(['email', 'token', 'expires_at'])
+        ->first();
+
+    $this->get(route('admin.reset-password.get', ['token' => $resetTokens->token]))
+        ->assertStatus(Response::HTTP_OK);
+});
+
 todo("send email when found user");
 
 # SECOND STEP
