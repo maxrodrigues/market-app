cbe12ec7abd618726c729de53dbcb35e06d6ac54
Max Rodrigues <maxuel.rodrigues@gmail.com>
2025-12-04 12:25:06 -0300

    [feat] ✨: Criando menu do painel administrador

---
+++ resources/views/components/navbar/nav-item.blade.php
@@ -0,0 +1,7 @@
+<div class="w-full py-3 pl-3 text-gray-600 hover:bg-sky-300 hover:rounded-lg hover:text-sky-700">
+    <a href="#" class="">
+        <x-dynamic-component :component="$icon"/>
+        <i class="fa-solid fa-house text-xl mr-2"></i>
+        <span>{{ $name }}</span>
+    </a>
+</div>
---
+++ resources/views/pages/admin/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-layouts.admin-page>
+
+</x-layouts.admin-page>
