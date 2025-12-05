ded47d5d1c38996eb88952763c91c19de0c512fa
Max Rodrigues <maxuel.rodrigues@gmail.com>
2025-12-05 12:08:25 -0300

    [test] 🧪: Criação de todos em pt-br para organização

---
+++ tests/Feature/Admin/DashboardTest.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Organizar os testes com relação à exibição de dados.
+ * O filtro padrão nesse momento será de 7 dias.
+ */
+
+todo('Total de clientes cadastrados nos ultimos 7 dias');
+todo('total de pedidos feitos nos ultimos 7 dias');
+todo('total faturado nos ultimos 7 dias');
+todo('total de produtos com estoque baixo');
+todo('ultimos 10 pedidos realizados');
+todo('5 produtos com estoque baixo');
