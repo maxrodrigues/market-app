9725d5a1dbea7bbc1d89e6c3ee26971cab95c29e
Max Rodrigues <maxuel.rodrigues@gmail.com>
2025-12-10 14:58:48 -0300

    [feat] ✨: Formulário de cadastro de produtos

---
+++ resources/views/pages/admin/products/view.blade.php
@@ -0,0 +1,63 @@
+<x-layouts.admin-page>
+    <!-- -->
+    <div class="flex justify-between items-center">
+        <div class="flex flex-col">
+            <h1 class="text-3xl font-bold mb-1">Add New Product</h1>
+            <span class="text-sm text-gray-600">Fill in the details below to add a new product to the catalog.</span>
+        </div>
+    </div>
+    <div class="mt-10 grid grid-cols-12 gap-4">
+        <div class="col-span-9">
+            <!-- -->
+            <div class="bg-white flex flex-col items-start rounded-lg mb-8">
+                <div class="flex justify-between py-3 px-5 w-full space-x-3 border-b border-gray-400">
+                    <span class="font-bold text-xl">Product Information</span>
+                </div>
+                <div>
+
+                </div>
+            </div>
+
+            <!-- -->
+            <div class="bg-white flex flex-col items-start rounded-lg mb-8">
+                <div class="flex justify-between py-3 px-5 w-full space-x-3 border-b border-gray-400">
+                    <span class="font-bold text-xl">Variants</span>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-span-3">
+            <!-- -->
+            <div class="bg-white flex flex-col items-start rounded-lg mb-8">
+                <div class="flex justify-between py-3 px-5 w-full space-x-3 border-b border-gray-400">
+                    <span class="font-bold text-xl">Media</span>
+                </div>
+                <div>
+
+                </div>
+            </div>
+
+            <!-- -->
+            <div class="bg-white flex flex-col items-start rounded-lg mb-8">
+                <div class="flex justify-between py-3 px-5 w-full space-x-3 border-b border-gray-400">
+                    <span class="font-bold text-xl">Price & Inventory</span>
+                </div>
+                <div>
+
+                </div>
+            </div>
+
+            <!-- -->
+            <div class="bg-white flex flex-col items-start rounded-lg mb-8">
+                <div class="flex justify-between py-3 px-5 w-full space-x-3 border-b border-gray-400">
+                    <span class="font-bold text-xl">Status</span>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layouts.admin-page>
